478c1f9924ae38cebb4f320829017c8a2e99ac34
Mike Hansford <mjhansford@gmail.com>
2025-10-15 21:51:22 +1100

    Adding Seed project

---
+++ EFCoreDemo.DbSeed/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using EFCoreDemo.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello world");
+
+var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((ctx, svcs) =>
+            {
+                svcs.AddDbContext<FootballLeagueDbContext>(opts => 
+                    opts.UseNpgsql(ctx.Configuration.GetConnectionString("PgCn")));
+            })
+            .Build();
+
+// write to the DB here
+using var context = host.Services.GetRequiredService<FootballLeagueDbContext>();
+
+await context.Database.EnsureCreatedAsync();
+
+var testawait context.Leagues.FirstOrDefaultAsync(l => l.Name = "AFL");
+if ()
+
+
+await host.RunAsync();
